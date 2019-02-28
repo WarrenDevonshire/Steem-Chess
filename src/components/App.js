4f334024aabbd7b692c2e71f946f210bb03a010a
Warren Devonshire <wdevonshire@icloud.com>
2019-02-28 16:42:28 -0500

    Fixed merge from develop

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,7 @@ import './App.css';
 import Header from '../shared/components/layout/Header';
 import Footer from '../shared/components/layout/Footer';
 import Content from '../shared/components/layout/Content';
-<<<<<<< HEAD
-import List from './List/List';
 import Menubar from '../shared/components/Menu/Menubar'
-=======
->>>>>>> develop
 
 class App extends Component {
   render() {
@@ -16,12 +12,7 @@ class App extends Component {
         <Header title="Welcome to Steem-Chess" />
 
         <Content>
-<<<<<<< HEAD
           <Menubar/>
-          <Home />
-=======
-          <h1>This is a test.</h1>
->>>>>>> develop
         </Content>
 
         <Footer />
