cd11842a8e3a07bd9e3e1d215c1022c784fa63c4
Matthew Halloran <Halloranm8@students.rowan.edu>
2019-02-28 16:22:51 -0500

    Box around create game

--- src/components/Create Game/CreateGame.js
+++ src/components/Create Game/CreateGame.js
@@ -10,23 +10,37 @@ class CreateGame extends Component{
         return (
             <div className="CreateGame">
                 <h1>Create Game</h1>
-                <p>
-                    <Slider/>
-                </p>
-                <Slider/>
-                <p>
-                    Paragraph Example
-                </p>
-                <img class="img1" 
-                src={BlackPiece}
-                alt="Black"/>
-                <img class="img1" 
-                src={MixedPiece}
-                alt="Random"/>
-                <img class="img1" 
-                src={WhitePiece}
-                alt="White"/>
-                <button>Start</button>
+                <h1>Join Game</h1>
+                <div className="mainDiv">
+                    <div className="leftBox box">
+                        <h3>Time Per Side</h3>
+                        <h3>Increment</h3>
+                            <Slider/>
+                        <h3>Starting Color</h3>
+                        <ul className="horizontal">
+                            <li>
+                            <img className="img1" 
+                        src={BlackPiece}
+                        alt="Black"/>
+                            </li>
+                            <li>
+                            <img className="img1" 
+                        src={MixedPiece}
+                        alt="Random"/>
+                            </li>
+                            <li>
+                            <img className="img1" 
+                        src={WhitePiece}
+                        alt="White"/>
+                            </li>
+                        </ul>
+                        <button>Start</button>
+                    </div>
+                    <div className="box">
+                        
+                        <button>Join</button>
+                    </div>
+                </div>
             </div>
         );
     }
