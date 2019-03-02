ba50106fd0f90653972b875ccb3921ee92b0d070
Matthew Halloran <Halloranm8@students.rowan.edu>
2019-03-01 23:47:23 -0500

    Knows what chess piece was clicked

--- src/components/Create Game/CreateGame.js
+++ src/components/Create Game/CreateGame.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import './CreateGame.css'
 import Slider from '../Slider/Slider'
 import WhitePiece from './Images/rook-white.png';
@@ -6,19 +7,29 @@ import MixedPiece from './Images/rook-mixed.png';
 import BlackPiece from './Images/rook-black.png';
 
 class CreateGame extends Component{
-    pieceChosen(e) {
-        e.preventDefault()
-        console.log(e.target)
-        var targetObject = e.target;
-        if(targetObject.className === "selectedItem")
-        {
-            targetObject.className = "chessItem";
+    constructor(props) {
+        super(props)
+        this.state = {
+          pieceChosen: -1,
+          mode: 0,
+          timePerSide: 10,
+          increment:  10
         }
-        else
-        {
-            targetObject.className = "selectedItem";
-        }
+        this.pieceChanged = this.pieceChanged.bind(this);
+        //this.addOne = this.addOne.bind(this)
+      }
+      
+    //   addOne() {
+    //     this.setState({
+    //       counter: this.state.counter + 1
+    //     })
+    //   }
+    pieceChanged(index) {
+        console.log(index);
+        //this.state.pieceChosen = index;
+        //console.log(this.state.pieceChosen);
     }
+
     render(){
         return (
             <div className="CreateGame">
@@ -35,20 +46,8 @@ class CreateGame extends Component{
                         <Slider/>
                         <hr noshade/>
                         <h3>Starting Color</h3>
-                        <span>
-                        <img onClick={e => this.pieceChosen(e)}
-                             className="chessItem" 
-                        src={BlackPiece}
-                        alt="Black"/>
-                            <img onClick={e => this.pieceChosen(e)}
-                            className="chessItem" 
-                        src={MixedPiece}
-                        alt="Random"/>
-                            <img onClick={e => this.pieceChosen(e)}
-                            className="chessItem" 
-                        src={WhitePiece}
-                        alt="White"/>
-                        </span>
+                        <PieceList indexClicked={this.state.pieceChosen}
+                                    onIndexChanged={this.pieceChanged}/>
                         <button>Start</button>
                     </div>
                     <div className="box half">
@@ -60,5 +59,61 @@ class CreateGame extends Component{
         );
     }
 }
+
+class PieceList extends Component{
+    constructor(props){
+        super(props);
+        const colorChoices = [[0, BlackPiece, "Black"], [1, MixedPiece, "Random"], [2, WhitePiece, "White"]];
+        this.state = {
+            indexChosen: -1,
+            pieces: colorChoices.map(([index, file, tag]) =>
+            <img key={index}
+            id={index}
+            onClick={e => this.pieceClicked(e)}
+            className="chessPiece"
+            src={file} 
+            alt={tag} />
+            )
+        }
+    }
+
+    pieceClicked(e) {
+        if(this.state.indexChosen == -1 || this.state.indexChosen != e.target.id)
+        {
+            this.state.indexChosen = e.target.id;
+            for(var i = 0; i < this.state.pieces.length; i++) {
+                if(i == e.target.id) {
+                    e.target.className = "selectedPiece";
+                    console.log("a" + i)
+                }
+                else {
+                    e.target.className = "chessPiece";
+                    console.log("b" + i)
+                }
+            }
+        }
+        else
+        {
+            this.state.indexChosen = -1;
+            for(var i = 0; i < this.state.pieces.length; i++) {
+                e.target.className = "chessPiece";
+                console.log("d" + i)
+            }
+        }
+        this.props.onIndexChanged(this.state.indexChosen);
+    };
+
+    render() {
+        return (
+            <span>
+                {this.state.pieces}
+            </span>
+        );
+    }
+}
+
+// ReactDOM.render(
+//     <PieceList />,
+//     document.getElementById('root'))
 
 export default CreateGame;
