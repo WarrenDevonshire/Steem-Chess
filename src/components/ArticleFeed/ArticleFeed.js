23ffc25884556246c8bf1c4ae8f9a636765be0da
redgf8 <redgf8@users.noreply.github.com>
2019-03-04 20:39:51 -0500

    Basic implementation of article feed.

--- src/components/ArticleFeed/ArticleFeed.js
+++ src/components/ArticleFeed/ArticleFeed.js
@@ -1,14 +1,48 @@
 import React, {Component} from 'react';
 import './ArticleFeed.css';
+import { Client } from 'dsteem';
 
-class ArticleFeed extends Component{
+const client = new Client('https://api.steemit.com');
+
+function fetchBlog(limit, sortMethod) {
+    
+    const query = {
+        tag: 'chess',
+        limit: 10,
+    };
+    
+    client.database
+        .getDiscussions('trending', query)
+        .then(result => {
+            var posts = [];
+            result.forEach(post => {
+                const json = JSON.parse(post.json_metadata);
+                const image = json.image ? json.image[0] : '';
+                const title = post.title;
+                const author = post.author;
+                const created = new Date(post.created).toDateString();
+                posts.push(
+                    `<div class="list-group-item"><h4 class="list-group-item-heading">${title}</h4>
+                    <p>by ${author}</p><center><img src="${image}" class="img-responsive center-block" style="max-width: 450px"/></center>
+                    <p class="list-group-item-text text-right text-nowrap">${created}</p></div>`
+                );
+            });
+
+            document.getElementById('postList').innerHTML = posts.join('');
+        
+        })
+        .catch(err => {
+            alert('Error occured' + err);
+        });
+}
+
+export default class ArticleFeed extends Component{
     render(){
         return (  
             <div className="ArticleFeed">
-                <h1>Articles will go here.</h1>
+                <div class="list-group" id="postList"></div>
+                {fetchBlog("trending", 10)}
             </div>
         )
     }
 }
-
-export default ArticleFeed;
