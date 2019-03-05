36a5748fc946bf777735865df3bd50b1ec9e0130
Warren Devonshire <wdevonshire@icloud.com>
2019-03-05 17:28:47 -0500

    Added chessboardjsx component

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Header from '../shared/components/layout/Header';
 import Footer from '../shared/components/layout/Footer';
 import Content from '../shared/components/layout/Content';
-
+import ChessGame from './ChessGame/ChessGame'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header title="Welcome to Steem-Chess" />
-
+        <ChessGame/>
         <Content>
           <h1>This is a test.</h1>
         </Content>
