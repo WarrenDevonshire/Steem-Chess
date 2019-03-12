7ff8703eb66123e19c3b710918f5593e549259fa
redgf8 <redgf8@users.noreply.github.com>
2019-03-12 16:21:22 -0400

    Add push comment function.

--- src/components/Post/CommentFeed/CommentFeed.js
+++ src/components/Post/CommentFeed/CommentFeed.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './CommentFeed.css';
-import { Client } from 'dsteem';
+import { Client, PrivateKey } from 'dsteem';
 import { Testnet as NetConfig } from '../../../configuration';
 
 let opts = { ...NetConfig.net };
@@ -9,7 +9,7 @@ const fetchClient = new Client('https://api.steemit.com');
 const pushClient = new Client(NetConfig.url, opts);
 const Remarkable = require('remarkable');
 
-export default class Post extends Component{
+export default class Post extends Component {
 
     constructor(props) {
 
@@ -27,8 +27,8 @@ export default class Post extends Component{
     }
 
     fetchComments() {
-    
-            fetchClient.database
+
+        fetchClient.database
             .call('get_content_replies', [this.state.author, this.state.permlink]) // fetch post comments
             .then(result => {
 
@@ -40,13 +40,13 @@ export default class Post extends Component{
                     <div class="d-flex w-100 justify-content-between">\
                       <h5 class="mb-1">@${result[i].author}</h5>\
                       <small class="text-muted">${new Date(
-                          result[i].created
-                      ).toString()}</small>\
+                            result[i].created
+                        ).toString()}</small>\
                     </div>\
                     <p class="mb-1">${md.render(result[i].body)}</p>\
                     <small class="text-muted">&#9650; ${
                         result[i].net_votes
-                    }</small>\
+                        }</small>\
                   </div>`
                     );
                 }
@@ -62,8 +62,50 @@ export default class Post extends Component{
 
     pushComment() {
 
-        alert("This will post a comment eventually.");
-    
+        //get private key
+        const privateKey = PrivateKey.fromString(
+            document.getElementById('postingKey').value
+        );
+        //get account name
+        const account = document.getElementById('username').value;
+        //get body
+        const body = document.getElementById('body').value;
+        //get parent author permalink
+        const parent_author = document.getElementById('parent_author').value;
+        //get parent author permalink
+        const parent_permlink = document.getElementById('parent_permlink').value;
+
+        //generate random permanent link for post
+        const permlink = Math.random()
+            .toString(36)
+            .substring(2);
+
+        const payload = {
+            author: account,
+            title: '',
+            body: body,
+            parent_author: parent_author,
+            parent_permlink: parent_permlink,
+            permlink: permlink,
+            json_metadata: '',
+        };
+
+        console.log('pustCSlient.broadcast.comment payload:', payload);
+        pushClient.broadcast.comment(payload, privateKey).then(
+            function (result) {
+                console.log('client.broadcast.comment response', result);
+                document.getElementById('postLink').style.display = 'block';
+                document.getElementById(
+                    'postLink'
+                ).innerHTML = `<br/><p>Included in block: ${
+                    result.block_num
+                    }</p><br/><br/><a href="http://condenser.steem.vc/@${parent_author}/${parent_permlink}">Check post here</a>`;
+            },
+            function (error) {
+                console.error(error);
+            }
+        );
+
     }
 
     clearFields() {
@@ -72,21 +114,21 @@ export default class Post extends Component{
         document.getElementById('parent_permlink').value = '';
     };
 
-    render(){
+    render() {
 
-        return (  
-            <div id="CommentFeed" class="container" id="content"><br/>
-    		    <h4>Submit a post to the Steem blockchain</h4>
-                Username: <input id="username" type="text" size="65" class="form-control" /><br/>
-                Posting private key: <input id="postingKey" type="password" size="65" class="form-control" /><br/>
-                Parent Author: <input id="parent_author" type="text" size="20" /><br/>
-                Parent Post Permalink: <input id="parent_permlink" type="text" size="20" /><br/>
-                Comment body:<br/>
-                <textarea id="body" class="form-control" rows="3">Content of the comment</textarea><br/>
+        return (
+            <div id="CommentFeed" class="container" id="content"><br />
+                <h4>Submit a post to the Steem blockchain</h4>
+                Username: <input id="username" type="text" size="65" class="form-control" /><br />
+                Posting private key: <input id="postingKey" type="password" size="65" class="form-control" /><br />
+                Parent Author: <input id="parent_author" type="text" size="20" /><br />
+                Parent Post Permalink: <input id="parent_permlink" type="text" size="20" /><br />
+                Comment body:<br />
+                <textarea id="body" class="form-control" rows="3">Content of the comment</textarea><br />
                 <input id="submitPostBtn" type="button" value="Submit comment!" onClick={() => this.pushComment()} class="btn btn-primary" />
                 <input id="clearFieldsBtn" type="button" value="Clear Fields" onClick={() => this.clearFields()} class="btn btn-primary" />
                 <div id="postComments" styles="display: none;" class="list-group"></div>
-		    </div>
+            </div>
         )
     }
 }
