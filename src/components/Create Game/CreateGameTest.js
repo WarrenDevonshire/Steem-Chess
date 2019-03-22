1823dae333c70dcd8e9e3de9a789fe79c9e30a4d
Matthew Halloran <Halloranm8@students.rowan.edu>
2019-03-21 22:35:35 -0400

    Copy and paste connecting functional
    Peers can connect through webRTC by copying and pasting the offer and answer. The messaging has not been set up yet

--- src/components/Create Game/CreateGameTest.js
+++ src/components/Create Game/CreateGameTest.js
@@ -4,6 +4,6 @@ import CreateGame from './CreateGame';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Game />, div);
+  ReactDOM.render(<CreateGame />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/LiveMatch/LiveMatch.js
+++ src/components/LiveMatch/LiveMatch.js
@@ -1,21 +1,77 @@
 
 import React, { Component } from 'react';
-import ChessGame from '../ChessGame/ChessGame';
 import Chatbox from '../Chatbox/Chatbox'
+import Peer from 'simple-peer';
 
-export default class LiveMatch extends Component {
+/**
+ * Component for playing a live chess match. Must
+ * be passed in the game data and if this user
+ * is initiating the webRTC connection
+ */
+class LiveMatch extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            peer: new Peer({ initiator: this.props.isInitiating, trickle: false }),
 
-        render(){
-
-            return (
-
-                <div id="liveMatch">
-                    {/* <ChessGame/> */}
-                    <Chatbox localConnection={this.props.localConnection}/>                
-                </div>
-
-            )
-
+            //test data
+            rtcText: ""
         }
 
+        console.log(this.props.isInitiating);
+
+        this.state.peer.on('error', (err) => { 
+            console.log('error', err) 
+        });
+
+        this.state.peer.on('signal', (data) => {
+            console.log('SIGNAL', JSON.stringify(data));
+        });
+
+        this.state.peer.on('connect', () => {
+            console.log('CONNECT', this.state);
+            this.state.peer.send('whatever' + Math.random())
+        });
+
+        this.state.peer.on('data', (data) => {
+            console.log('data: ' + data);
+        });
+
+        //Test methods
+        this.testChanged = this.testChanged.bind(this);
+        this.testClick = this.testClick.bind(this);
+        //End test methods
+    }
+
+    //Test methods
+
+    testChanged(e) {
+        this.setState({ rtcText: e.target.value });
+    }
+
+    testClick() {
+        this.state.peer.signal(this.state.rtcText);
+    }
+
+    //End test methods
+
+    render() {
+
+        return (
+
+            <div id="liveMatch">
+                {/* For testing rtc */}
+                <textarea onChange={e => this.testChanged(e)} />
+                <button onClick={e => this.testClick()}>TESTTTT</button>
+                {/* End testing area */}
+                {/* <ChessGame/> */}
+                <Chatbox localConnection={this.props.localConnection} />
+            </div>
+
+        )
+
+    }
+
 }
+
+export default LiveMatch;
