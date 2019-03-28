9d37e3e64c16f7b8fd2fd63f20a6bf43e77c3340
redgf8 <redgf8@users.noreply.github.com>
2019-03-28 11:59:43 -0400

    Remove articlefeed page code, moving to new branch.

--- src/components/ArticleFeed/ArticleFeed.js
+++ src/components/ArticleFeed/ArticleFeed.js
@@ -6,8 +6,6 @@ import { Link } from 'react-router-dom';
 
 const client = new Client('https://api.steemit.com');
 
-// TODO: figure out how to grab more articles for next page and add routing/url for pages, add page number in props
-
 export default class ArticleFeed extends Component {
 
     constructor(props) {
@@ -16,9 +14,9 @@ export default class ArticleFeed extends Component {
         
         this.state = {
 
-            pageNumber: 0, // this will store page number for browsing further articles in feed
-            posts: [],
-            activePosts: []
+            // this will store page number for browsing further articles in feed
+            pageNumber: 0,
+            posts: []
 
         };
 
@@ -28,26 +26,21 @@ export default class ArticleFeed extends Component {
 
     fetchBlog() {
         
-        // set up query
         const query = {
             tag: 'chess',
             limit: this.props.limit,
         };
-
-        let postList = [];
         
         client.database
-            .getDiscussions(this.props.sortMethod, query) // fetch posts
+            .getDiscussions(this.props.sortMethod, query)
             .then(result => {
     
                 result.forEach(post => {
                     
-                    postList.push(<PostPreview post={post} />);
+                    this.state.posts.push(<PostPreview post={post} />);
 
                 });
 
-                this.setState( { posts: postList } );
-                this.setState( { activePosts: postList.slice(0, 9) })
                 this.forceUpdate();
             
             })
@@ -60,13 +53,11 @@ export default class ArticleFeed extends Component {
 
     }
 
-    // go back to previous page of posts
     prevPage() {
 
         if (this.state.pageNumber > 0) {
 
             this.setState({ pageNumber: this.state.pageNumber - 1 });
-            this.updateActivePosts();
 
         } else {
 
@@ -77,52 +68,26 @@ export default class ArticleFeed extends Component {
     
     }
 
-    // go to next page of posts
     nextPage() {
 
-        if (this.state.pageNumber < 10) {
-
-            this.setState({ pageNumber: this.state.pageNumber + 1 });
-            this.updateActivePosts();
-
-        } else {
-
-            alert("Already at last page!");
-
-        }
+        this.setState({ pageNumber: this.state.pageNumber + 1 })
     
     }
 
-    // switch out posts displayed on page when going to a new page on the feed
-    updateActivePosts() {
-
-        this.setState({ activePosts: this.state.posts.slice((this.state.pageNumber * 10), (((this.state.pageNumber + 1) * 10) - 1)) });
-        this.forceUpdate();
-
-    }
-
     componentDidMount() {
-
-        //set default page to 0 (1 for user)
-        this.setState({ pageNumber: 0 });
-
-    }
+        this.setState({ pageNumber: 0});
+      }
 
     render() {
 
         return (  
 
-            <div id="ArticleFeed" className="ArticleFeed">
-
+            <div className="ArticleFeed">
                 <Link to="/Compose"><button>Compose New Article</button></Link>
-                <hr />
+                <div class="list-group" id="postList">{this.state.posts.map(PostPreview => <div> {PostPreview} </div>)}</div>
                 <button id="PrevPage" onClick={() => this.prevPage()}>Previous Page</button>
-                <p>Page {this.state.pageNumber + 1}</p>
+                {this.state.pageNumber}
                 <button id="NextPage" onClick={() => this.nextPage()}>Next Page</button>
-                <hr />
-                <div class="list-group" id="postList">{this.state.activePosts.map(PostPreview => <div> {PostPreview} <hr /></div>)}</div>
-                <p>Page {this.state.pageNumber + 1}</p>    
-
             </div>
 
         )
