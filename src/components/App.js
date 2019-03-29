f0ca2a59702c400e8abea9c32668f8561bf04936
Warren Devonshire <wdevonshire@icloud.com>
2019-03-29 17:53:14 -0400

    Remove steemconnect code from App.js.

--- src/components/App.js
+++ src/components/App.js
@@ -5,47 +5,24 @@ import Footer from '../shared/components/layout/Footer';
 import Content from '../shared/components/layout/Content';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import ArticleFeed from './ArticleFeed/ArticleFeed';
-import Game from './Game/Game';
 import LiveMatch from './LiveMatch/LiveMatch';
 import Post from './Post/Post';
 import Compose from './Compose/Compose';
 import Success from './Success/Success';
-import sc2 from "steemconnect";
 import Play from './Play/Play';
+
 class App extends Component {
     constructor(props) {
         super(props);
-        let api = sc2.Initialize({
-            app: 'SteemChess',
-            callbackURL: 'https://localhost:3000/Success',
-            accessToken: 'access_token',
-            scope: ['vote', 'comment']
-        });
 
-        this.onAccessToken = this.onAccessToken.bind(this);
-        this.getAccessToken = this.getAccessToken.bind(this);
-        this.state = {
-            api: api,
-            access_token: null
-        };
     }
 
-    onAccessToken(token) {
-        this.setState(() => {
-            return {access_token: token};
-        });
-    }
-
-    getAccessToken() {
-        return this.state.access_token;
-    }
 
     render() {
         return (
             <Router>
                 <div className="App">
                     <Header/>
-                    <p>{this.state.access_token}</p>
                     <Content>
                         <Route path='/'
                                render={(props) => <ArticleFeed {...props} limit={'10'} sortMethod={'trending'}/>}
@@ -54,18 +31,12 @@ class App extends Component {
                                exact/>
                         <Route path='/New'
                                render={(props) => <ArticleFeed {...props} limit={'10'} sortMethod={'created'}/>} exact/>
-                        <Route path='/Play' render={(props) => <Play {...props} getAccessToken={this.getAccessToken}/>}
-                               exact/>
-                        <Route path='/Live'
-                               render={(props) => <LiveMatch {...props} getAccessToken={this.getAccessToken}/>} exact/>
+                        <Route path='/Play' render={(props) => <Play {...props} />} exact/>
+                        <Route path='/Live' render={(props) => <LiveMatch {...props} />} exact/>
                         <Route path='/Post/@:author/:permlink' component={Post} exact/>
                         <Route path='/Compose' component={Compose} exact/>
-                        <Route path='/Success'
-                               render={(props) => <Success {...props} api={this.state.api}
-                                                           onAccessToken={this.onAccessToken}/>}/>
-                        <Route path='/Test' component={Play}/>
+                        <Route path='/Success' render={(props) => <Success {...props} api={this.state.api}/>}/>
                     </Content>
-
                     <Footer/>
                 </div>
             </Router>
