9666150023719c5ab5dfa7da9f029932c4722b56
Warren Devonshire <wdevonshire@icloud.com>
2019-05-14 23:51:24 -0400

    Updates

---
+++ precache-manifest.082e089e764cf208161c9484883e0925.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "17b6a9693d6e6d9ce0fca068a90d3fbb",
+    "url": "/Steem-Chess/static/media/Draw.17b6a969.png"
+  },
+  {
+    "revision": "2b02805c63d1b43bf7f0",
+    "url": "/Steem-Chess/static/css/main.74948e8f.chunk.css"
+  },
+  {
+    "revision": "2c5484781b2c853b028c",
+    "url": "/Steem-Chess/static/js/runtime~main.214861b4.js"
+  },
+  {
+    "revision": "5fe4039b11fa6d042519dd1d049901c9",
+    "url": "/Steem-Chess/static/media/spinner.5fe4039b.gif"
+  },
+  {
+    "revision": "1dee7ea72eb6f2462785",
+    "url": "/Steem-Chess/static/js/2.2ae57648.chunk.js"
+  },
+  {
+    "revision": "301ce772bbf20538917e651f75419aea",
+    "url": "/Steem-Chess/static/media/no-image.301ce772.png"
+  },
+  {
+    "revision": "2b02805c63d1b43bf7f0",
+    "url": "/Steem-Chess/static/js/main.f694251e.chunk.js"
+  },
+  {
+    "revision": "5997b080a0badc197bf6505e8da351e1",
+    "url": "/Steem-Chess/static/media/Resign.5997b080.png"
+  },
+  {
+    "revision": "3d52ae776415d716543ee208ce458341",
+    "url": "/Steem-Chess/static/media/rook-black.3d52ae77.png"
+  },
+  {
+    "revision": "f81d7ebe0db3b4b82e065e10a2b4573b",
+    "url": "/Steem-Chess/static/media/rook-mixed.f81d7ebe.png"
+  },
+  {
+    "revision": "285f87a2141da39c89b89bdd44847bf6",
+    "url": "/Steem-Chess/static/media/rook-white.285f87a2.png"
+  },
+  {
+    "revision": "1dee7ea72eb6f2462785",
+    "url": "/Steem-Chess/static/css/2.c75c92de.chunk.css"
+  },
+  {
+    "revision": "b42ea07db4bcbfa104be07f5ba708029",
+    "url": "/Steem-Chess/index.html"
+  }
+];
